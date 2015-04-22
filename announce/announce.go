acd925b03e2a4f5c8ada05fc94aed4189cbf7fa3
Falk Hoppe <falkhoppe81@gmail.com>
2015-04-22 15:46:58 +0200

    fixed service announcement

--- announce/announce.go
+++ announce/announce.go
@@ -4,12 +4,32 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-func requestServiceAnnouncement(overseerRoot string, service []byte) {
+func postService(address string, service []byte, donechan chan bool, errorchan chan error, checkchan chan bool) {
+	resp, err := http.Post(address, "application/json", bytes.NewBuffer(service))
+	select {
+	case <-donechan:
+		return
+	default:
+		if err != nil {
+			errorchan <- err
+			return
+		}
+		if resp.StatusCode > 299 {
+			errorchan <- fmt.Errorf("Request Error: %s", resp.Status)
+			return
+		}
+
+		checkchan <- true
+	}
+}
+
+func requestServiceAnnouncement(overseerAddress string, service []byte) {
 	go func() {
 		available := true
 
@@ -25,39 +45,22 @@ func requestServiceAnnouncement(overseerRoot string, service []byte) {
 
 		for available {
 
-			go func() {
-				resp, err := http.Post(overseerRoot, "application/json", bytes.NewBuffer(service))
-				select {
-				case <-donechan:
-					return
-				default:
-					if err != nil {
-						errorchan <- err
-						return
-					}
-					if resp.StatusCode > 299 {
-						errorchan <- fmt.Errorf("Request Error: %s", resp.Status)
-						return
-					}
-
-					checkchan <- true
-				}
-			}() // end request routine
+			go postService(overseerAddress, service, donechan, errorchan, checkchan) // end request routine
 
 			select {
 			case <-checkchan:
 				// JUST GO ON
 			case err := <-errorchan:
-				fmt.Println(err)
+				log.Println(err)
 				time.Sleep(2 * time.Second)
-				requestServiceAnnouncement(overseerRoot, service)
+				requestServiceAnnouncement(overseerAddress, service)
 				available = false
 				return
 			case <-time.After(time.Second * 3):
 				donechan <- true
-				fmt.Errorf("Registering new Service failed with timeout")
+				log.Printf("Registering new Service failed with timeout")
 				time.Sleep(2 * time.Second)
-				requestServiceAnnouncement(overseerRoot, service)
+				requestServiceAnnouncement(overseerAddress, service)
 			}
 
 			time.Sleep(10 * time.Minute) // renewal registration once in a time, in case the overseer service failed
@@ -66,14 +69,17 @@ func requestServiceAnnouncement(overseerRoot string, service []byte) {
 	}() // end loop routine
 }
 
-// NewService provides a method to attach a new Service to the overseer stack
-func NewService(serviceName string) {
+// Service provides a method to attach a new Service to the overseer stack
+func Service(serviceName string) {
 	serviceRoot := os.Getenv("ROOT_URL")
 	aliveResource := os.Getenv("ALIVE_URL")
-	overseerRoot := os.Getenv("OVERSEER_ROOT")
 
-	if serviceRoot == "" || aliveResource == "" || overseerRoot == "" {
-		fmt.Println("ROOT_URL and/or ALIVE_URL and/or OVERSEER_ROOT not set")
+	overseerHost := os.Getenv("OVERSEER_HOST")
+	overseerPort := os.Getenv("OVERSEER_PORT")
+	overseerAddress := overseerHost + ":" + overseerPort
+
+	if serviceRoot == "" || aliveResource == "" || overseerHost == "" || overseerPort == "" {
+		log.Println("ROOT_URL and/or ALIVE_URL and/or OVERSEER_ROOT not set")
 		return
 	}
 
@@ -88,5 +94,5 @@ func NewService(serviceName string) {
 	})
 
 	fmt.Printf("Announcing new Service to Overseer: %s", service)
-	requestServiceAnnouncement(overseerRoot, service)
+	requestServiceAnnouncement(overseerAddress, service)
 }
